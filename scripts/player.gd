0107318cd81b6de32dd6752dbf468ad0a8932df9
Cubinx09 <lalaneamanda0923@gmail.com>
2025-10-25 19:15:07 -0400

    Add files via upload
    messed something up and now we have to start ALLLLLL OVVVVERRRR. COME ON!!!!!

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,35 +1,20 @@
 extends CharacterBody2D
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -250.0
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	# flips the sprite
-	if direction > 0: 
-		animated_sprite_2d.flip_h = false
-	elif direction < 0:
-		animated_sprite_2d.flip_h = true
-	# play different animations
-	if is_on_floor():
-		if direction  == 0:
-			animated_sprite_2d.play('Idle')
-		else:
-			animated_sprite_2d.play("Run")
-	else:
-		animated_sprite_2d.play("Jump")
-	
-	
-		
-	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
