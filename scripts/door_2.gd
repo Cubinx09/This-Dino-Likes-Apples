6c921c8f99f11f8cc26b9f5f9ffd806a5b3e665c
Cubinx09 <lalaneamanda0923@gmail.com>
2025-10-29 22:59:32 -0400

    Add files via upload

--- scripts/door_2.gd
+++ scripts/door_2.gd
@@ -1,5 +1,5 @@
 extends Area2D
-const FILE = "res://scenes/level_2.tscn"
+const FILE = "res://scenes/level_3.tscn"
 
 func _process(_body):
 	if Input.is_action_pressed("enter"):
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,11 +1,8 @@
 extends Area2D
-var player = "player.tscn"
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(_body):
 	timer.start()
-	Engine.time_scale = 0.5
-	player.get_node("CollisionShape2D").queue_free()
 	print("ouch!")
 	
 func _on_timer_timeout():
