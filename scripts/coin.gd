fa27ef0cdc34a7efa822f9a1091b277cdc77a2dc
Cubinx09 <lalaneamanda0923@gmail.com>
2025-10-31 23:39:02 -0400

    Delete scripts directory

--- scripts/coin.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-
-func _on_body_entered(_body):
-	queue_free()
-	print("+1 moneys")
--- scripts/door_2.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-const FILE = "res://scenes/level_3.tscn"
-
-func _process(_body):
-	if Input.is_action_pressed("enter"):
-		get_tree().change_scene_to_file(FILE)
-	
-		
